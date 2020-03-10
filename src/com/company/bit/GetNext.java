32ff7fd33d80a8a2a9f3c2cb1b11066c0ad8fe43
hl007 <18896558387@163.com>
2020-03-10 13:26:53 +0800

    完善位操作

--- src/com/company/bit/GetNext.java
+++ src/com/company/bit/GetNext.java
@@ -5,71 +5,73 @@ import java.util.*;
 
 // 给定一个正整数，返回与其二进制中1的个数相同且大小最接近的两个数（一个略大，一个略小）
 public class GetNext {
-    public static int[] getNext(int x) {
+    // 获取较大的数：先找到右边非拖尾的0，然后将其右边的0和1重新排列
+    public static int getNext(int x) {
         if(x<=0) throw new IllegalArgumentException("x必须为正整数");
 
-        // 找出最低位的0
-        boolean[] a=new boolean[32];
-        for(int i=0;i<32;i++) {
-            a[31-i]=getBit(x,i);
-        }
-        int lowIndex=0;
-        for(int i=0;i<32;i++) {
-             if(!a[i]) lowIndex=i;
+        int c=x;
+        int c0=0;  // 非拖尾的0右边0的数量
+        int c1=0;  // 非拖尾的0右边1的数量
+        while((c&1)==0) {  // 计算非拖尾的0右边0的数量
+            c0++;
+            c>>=1;
         }
 
-        // 计算1的个数
-        ArrayList<Integer> arrOf1=new ArrayList<>();
-        for(int i=0;i<32;i++) {
-            if(a[i]) arrOf1.add(i);
+        while((c&1)==1) {  // 计算非拖尾的0右边1的数量
+            c1++;
+            c>>=1;
         }
 
-        if(arrOf1.size()==1) {
-            return new int[]{2,4};
-        }
-        else if(arrOf1.size()==2) {
-            int temp=setBit(x,arrOf1.get(0),true);
-            int closet=setBit(temp,arrOf1.get(0),false);
-            int closet2=setBit(temp,arrOf1.get(1),false);
-
-            return new int[]{closet,closet2};
-        }
-        else {
-            HashMap<Integer,Integer> map=new HashMap<>();
-            for(int i:arrOf1) {
-                map.put(i,Math.abs(i-lowIndex));
-            }
-
-            System.out.println("map: "+map);
-
-            // 根据值排序
-            List<Map.Entry<Integer,Integer>> list = new ArrayList<Map.Entry<Integer,Integer>>(map.entrySet());
-            Collections.sort(list,new Comparator<Map.Entry<Integer,Integer>>() {
-                public int compare(Map.Entry<Integer, Integer> o1,
-                                   Map.Entry<Integer, Integer> o2) {
-                    return o1.getValue().compareTo(o2.getValue());
-                }
-            });
-
-            int temp=setBit(x,arrOf1.get(0),true);
-            int closet=setBit(temp,list.get(0).getKey(),false);
-            int closet2=setBit(temp,list.get(1).getKey(),false);
-            int closet3=setBit(temp,list.get(2).getKey(),false);
-
-            HashMap<Integer,Integer> map2=new HashMap<>();
-            map2.put(closet,Math.abs(closet-x));
-            map2.put(closet2,Math.abs(closet2-x));
-            map2.put(closet3,Math.abs(closet3-x));
-            List<Map.Entry<Integer,Integer>> list2 = new ArrayList<Map.Entry<Integer,Integer>>(map2.entrySet());
-            Collections.sort(list2,new Comparator<Map.Entry<Integer,Integer>>() {
-                public int compare(Map.Entry<Integer, Integer> o1,
-                                   Map.Entry<Integer, Integer> o2) {
-                    return o1.getValue().compareTo(o2.getValue());
-                }
-            });
-            return new int[]{list.get(0).getKey(),list.get(1).getKey()};
+        if(c0+c1==31) {  // 0111110000...，不存在更大且1相同的值
+            return -1;
         }
 
+        int p=c0+c1;
+        x=setBit(x,p,true);  // p位设为1
+        // 重新排列p的右边
+        for(int i=0;i<c1-1;i++) {
+            x=setBit(x,i,true);
+        }
+        for(int i=c1-1;i<p;i++) {
+            x=setBit(x,i,false);
+        }
+
+        return x;
+    }
+
+    // 获取较小的数：先找到右边非拖尾的1，然后将其右边的0和1重新排列
+    public static int getPrev(int x) {
+        if(x<=0) throw new IllegalArgumentException("x必须为正整数");
+
+        int c=x;
+        int c0=0;
+        int c1=0;
+
+        while((c&1)==1) {
+            c1++;
+            c>>=1;
+        }
+
+        while((c&1)==0) {
+            c0++;
+            c>>=1;
+        }
+
+        if(c0+c1==32) {  // 00000000111...，不存在更小且1相同的值
+            return -1;
+        }
+
+        int p=c0+c1;
+        x=setBit(x,p,false);  // p位设为0
+        // 重新排列p的右边
+        for(int i=0;i<c0-1;i++) {
+            x=setBit(x,i,false);
+        }
+        for(int i=c0-1;i<p;i++) {
+            x=setBit(x,i,true);
+        }
+
+        return x;
     }
 
     // 获取数位
@@ -83,10 +85,10 @@ public class GetNext {
         int mask=~(1<<i);
         return (num & mask) | (val<<i);
     }
-
-
+    
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(123456));
-        System.out.println(Arrays.toString(getNext(123456)));
+        System.out.println(Integer.toBinaryString(1234554));
+        System.out.println(Integer.toBinaryString(getPrev(1234554)));
+        System.out.println(getPrev(1234554));
     }
 }
