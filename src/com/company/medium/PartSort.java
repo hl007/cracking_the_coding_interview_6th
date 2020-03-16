f7118b7e85535e0c323529412a90c26512e15f78
hl007 <18896558387@163.com>
2020-03-16 15:02:40 +0800

    完善中等难题

--- src/com/company/medium/PartSort.java
+++ src/com/company/medium/PartSort.java
@@ -1,17 +1,69 @@
 package com.company.medium;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 // 给定一个数组a，找出最短序列a[m:n+1]，使得此序列有序后，整个数组也有序
 public class PartSort {
     public static int[] partSort(int[] a) {
-        int m=0;
-        int n=a.length-1;
-        while(m<=n) {
-            int[] prev=Arrays.copyOfRange(a,0,m-1);
+        int[] result=partSortHelper(a);
 
+        int[] reverse=new int[a.length];
+        for(int i=0;i<a.length;i++) {
+            reverse[a.length-1-i]=a[i];
         }
-        return null;
+        int[] result2=partSortHelper(reverse);
+        if(result2!=null) {
+            result2=new int[]{a.length-1-result2[1],a.length-1-result2[0]};
+        }
+
+        if(result==null) {
+            return result2;
+        }
+        else {
+            return result;
+        }
+    }
+
+    private static int[] partSortHelper(int[] a) {  // 升序
+        if(a.length<=2) {
+            return null;
+        }
+
+        int m=0;
+        while(m<a.length-1) {  // 先确定m
+            int[] prev=Arrays.copyOfRange(a,0,m+1);
+            int[] last=Arrays.copyOfRange(a,m+1,a.length);
+            if(max(prev)<=min(last) && isSorted(prev) && a[1]-a[0]>=0) {
+                m++;
+            }
+            else {
+                break;
+            }
+        }
+
+
+        if(m==a.length-1) {
+            return null;
+        }
+
+        int n=a.length-1;
+        while(n>=m) {  // 再确定n
+            int[] prev=Arrays.copyOfRange(a,0,n);
+            int[] last=Arrays.copyOfRange(a,n,a.length);
+            if(max(prev)<=min(last) && isSorted(last) && (a[1]-a[0])*(a[a.length-1]-a[a.length-2])>=0) {
+                n--;
+            }
+            else {
+                break;
+            }
+        }
+
+        if(n==a.length-1) {
+            return null;
+        }
+
+        return new int[]{m,n};
     }
 
     private static int min(int[] a) {
@@ -27,7 +79,7 @@ public class PartSort {
     }
 
     // 判断是否有序
-    private boolean isSorted(int[] a) {
+    private static boolean isSorted(int[] a) {
         boolean flag=true;
         for(int i=0;i<a.length-1;i++) {
             if(i==0) {
@@ -45,7 +97,9 @@ public class PartSort {
     }
 
     public static void main(String[] args) {
-        int[] a=new int[]{1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
-        System.out.println();
+        int[] a=new int[]{1, 2, 4, 3, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+        int[] b=Arrays.copyOfRange(a,1,2);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(partSort(a)));
     }
 }
---
+++ src/com/company/medium/SumOfContinuousNumbers.java
@@ -0,0 +1,6 @@
+package com.company.medium;
+
+// 给定一个连续数组，返回总和最大的连续数列
+public class SumOfContinuousNumbers {
+
+}
